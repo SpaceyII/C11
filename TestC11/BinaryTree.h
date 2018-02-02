c36e07f91ab82028c9eb62dd9006b8cdb6584a4f
SpaceyII <windexale@163.com>
2018-02-02 15:45:38 +0800

    Add binary tree algorithm

---
+++ TestC11/BinaryTree.h
@@ -0,0 +1,11 @@
+#ifndef __BINARYTREE_H__
+#define __BINARYTREE_H__
+
+#include <string>
+
+using namespace std;
+//已知先序遍历和中序遍历求后序遍历 如先序ABC，中序BAC, 求得后序BCA
+string postOrder(string inOrder, string preOrder);
+
+
+#endif
